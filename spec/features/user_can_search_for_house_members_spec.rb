dea62c62175137071028f8c0c002445e1c1ecae0
Alex Robinson <alexandra.scott335@gmail.com>
2019-10-07 10:17:15 -0600

    feature test and view

---
+++ spec/features/user_can_search_for_house_members_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe "user can search for house members" do
+  scenario "by state" do
+    # As a user
+    # When I visit "/"
+    visit '/'
+    # And I select "Colorado" from the dropdown
+    select "Colorado", from: :state
+    # And I click on "Locate Members of the House"
+    click_on 'Locate Members from the House'
+    # Then my path should be "/search" with "state=CO" in the parameters
+    expect(current_path).to eq(search_path)
+    # And I should see a message "7 Results"
+    expect(page).to have_content("7 Results")
+    # And I should see a list of 7 the members of the house for Colorado
+    # And I should see a name, role, party, and district for each member
+    within(".members") do
+      expect(page).to have_css(".name")
+      expect(page).to have_css(".role")
+      expect(page).to have_css(".party")
+      expect(page).to have_css(".district")
+    end
+    # And they should be ordered by seniority from most to least
+
+  end
+end
+
+# propublica_api_key: 3ATWq2pq3mD1zn7dNtJH3Z7y01h19rqMOA1UDcuV
